6fbc51fbd6e86ac70eafb96d31fc22714a41ef50
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-16 14:56:41 +0300

    add argument to command to change time of schedule

--- kyiv_weather/management/commands/task_command.py
+++ kyiv_weather/management/commands/task_command.py
@@ -3,5 +3,17 @@ from kyiv_weather.tasks import main_task
 
 
 class Command(BaseCommand):
+    help = "Runs the main task with 9 hour by default, or specific hour if argument is passed"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "hour",
+            nargs="?",
+            type=str,
+            help="The hour in '24format' when the task should run",
+            default="9"
+        )
+
     def handle(self, *args, **options):
-        main_task()
+        hour = options.get("hour", "9")
+        main_task(hour=hour)
