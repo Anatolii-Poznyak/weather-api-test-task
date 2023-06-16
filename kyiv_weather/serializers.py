69b8f10afb16887c53935ba8b3f771b4fd613df1
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-16 10:22:06 +0300

    Add task serializer with default value on 9:00

--- kyiv_weather/serializers.py
+++ kyiv_weather/serializers.py
@@ -7,3 +7,11 @@ class WeatherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weather
         fields = ("id", "date", "temperature", "description")
+
+
+class TaskSerializer(serializers.Serializer):
+    minute = serializers.CharField(max_length=60, required=False, default="0")
+    hour = serializers.CharField(max_length=24, required=False, default="9")
+    day_of_week = serializers.CharField(max_length=7, required=False, default="*")
+    day_of_month = serializers.CharField(max_length=31, required=False, default="*")
+    month_of_year = serializers.CharField(max_length=12, required=False, default="*")
