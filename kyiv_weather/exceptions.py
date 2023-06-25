404b667c9498e732175f63ffc73b772425561b74
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-25 03:36:57 +0300

    Delete original exception from exceptions.py

--- kyiv_weather/exceptions.py
+++ kyiv_weather/exceptions.py
@@ -4,46 +4,37 @@ from django.utils.timezone import now
 class NetworkError(Exception):
     def __init__(
         self,
-        original_exception: Exception = None,
         message: str = "Network error occurred",
     ):
         self.message = message
         self.time = now()
-        self.original_exception = original_exception
         super().__init__(self.message)
 
     def __str__(self):
-        original_message = str(self.original_exception) if self.original_exception else ""
-        return f"{self.message} at {self.time}. Original exception: {original_message}"
+        return f"{self.message} at {self.time}."
 
 
 class ParsingError(Exception):
     def __init__(
         self,
-        original_exception: Exception = None,
         message: str = "Parsing error occurred",
     ):
         self.message = message
         self.time = now()
-        self.original_exception = original_exception
         super().__init__(self.message)
 
     def __str__(self):
-        original_message = str(self.original_exception) if self.original_exception else ""
-        return f"{self.message} at {self.time}. Original exception: {original_message}"
+        return f"{self.message} at {self.time}."
 
 
 class DataExtractionError(Exception):
     def __init__(
         self,
-        original_exception: Exception = None,
         message: str = "Data extraction error occurred",
     ):
         self.message = message
         self.time = now()
-        self.original_exception = original_exception
         super().__init__(self.message)
 
     def __str__(self):
-        original_message = str(self.original_exception) if self.original_exception else ""
-        return f"{self.message} at {self.time}. Original exception: {original_message}"
+        return f"{self.message} at {self.time}."
