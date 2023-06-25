30302ebfa089d98f758be0b0a157258bf4e90647
Anatolii Pozniak <a13101990a@gmail.com>
2023-06-25 04:40:56 +0300

    Add tests for scraper.py

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,28 +1,31 @@
 from unittest.mock import patch, Mock
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from requests.exceptions import RequestException
+
 from kyiv_weather.exceptions import NetworkError, ParsingError, DataExtractionError
 from kyiv_weather.models import Weather
 from kyiv_weather.scraper import parse_weather, save_weather, sync_weather
-from requests.exceptions import RequestException
 
 
 class ScraperTestCase(TestCase):
-    @patch('kyiv_weather.scraper.cloudscraper.create_scraper')
+    @patch("kyiv_weather.scraper.cloudscraper.create_scraper")
     def test_parse_weather_network_error(self, mock_scraper):
-        mock_scraper.side_effect = RequestException('Network error')
+        mock_scraper.side_effect = RequestException("Network error")
         with self.assertRaises(NetworkError):
             parse_weather()
 
-    @patch('kyiv_weather.scraper.BeautifulSoup')
-    @patch('kyiv_weather.scraper.cloudscraper.create_scraper')
+    @patch("kyiv_weather.scraper.BeautifulSoup")
+    @patch("kyiv_weather.scraper.cloudscraper.create_scraper")
     def test_parse_weather_parsing_error(self, mock_scraper, mock_soup):
-        mock_scraper.return_value.get.return_value.content = 'test'
-        mock_soup.side_effect = Exception('Parsing error')
+        mock_scraper.return_value.get.return_value.content = "test"
+        mock_soup.side_effect = Exception("Parsing error")
         with self.assertRaises(ParsingError):
             parse_weather()
 
-    @patch('kyiv_weather.scraper.BeautifulSoup')
-    @patch('kyiv_weather.scraper.cloudscraper.create_scraper')
+    @patch("kyiv_weather.scraper.BeautifulSoup")
+    @patch("kyiv_weather.scraper.cloudscraper.create_scraper")
     def test_parse_weather_data_extraction_error(self, mock_scraper, mock_soup):
         mock_soup.return_value.select.return_value = []
         with self.assertRaises(DataExtractionError):
@@ -30,13 +33,13 @@ class ScraperTestCase(TestCase):
                 Weather(date="2023-05-30", temperature=25, description="Sunny"),
                 Weather(date="2023-05-31", temperature=27, description="Cloudy"),
             ]
-            mock_scraper.return_value.get.return_value.content = 'test'
+            mock_scraper.return_value.get.return_value.content = "test"
             mock_soup.return_value.select.return_value = [Mock(), Mock()]
             parse_weather()
             save_weather(weather)
 
-    @patch('kyiv_weather.scraper.Weather.objects.update_or_create')
-    @patch('kyiv_weather.scraper.parse_weather')
+    @patch("kyiv_weather.scraper.Weather.objects.update_or_create")
+    @patch("kyiv_weather.scraper.parse_weather")
     def test_save_weather(self, mock_parse, mock_update_or_create):
         mock_weather = Mock(spec=Weather)
         mock_parse.return_value = [mock_weather]
@@ -49,9 +52,20 @@ class ScraperTestCase(TestCase):
             },
         )
 
-    @patch('kyiv_weather.scraper.save_weather')
-    @patch('kyiv_weather.scraper.parse_weather')
+    @patch("kyiv_weather.scraper.save_weather")
+    @patch("kyiv_weather.scraper.parse_weather")
     def test_sync_weather(self, mock_parse, mock_save):
         sync_weather()
         mock_parse.assert_called_once()
         mock_save.assert_called_once()
+
+    @patch("kyiv_weather.scraper.Weather.objects.update_or_create")
+    @patch("kyiv_weather.scraper.parse_weather")
+    def test_save_weather_raises_error_with_invalid_data(
+        self, mock_parse, mock_update_or_create
+    ):
+        mock_weather = Mock(spec=Weather, date="invalid_date")
+        mock_parse.return_value = [mock_weather]
+        mock_update_or_create.side_effect = ValidationError("Invalid data")
+        with self.assertRaises(ValidationError):
+            save_weather(mock_parse())
